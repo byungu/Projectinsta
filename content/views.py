40aea383057cab2ffbdc0a82a54a57e33b8e43af
byungu <100734050+byungu@users.noreply.github.com>
2022-05-26 22:13:28 +0900

    index

--- content/views.py
+++ content/views.py
@@ -7,7 +7,9 @@ from .models import Feed
 
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all() # select * from content_feed;
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed;
         for feed in feed_list:
 
             return render(request, "byungstagram/main.html", context=dict(feed_list=feed_list))
+
+    def
